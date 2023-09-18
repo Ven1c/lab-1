bb47882826b7b556855399083b0c14b9d60c99b2
Ven1c <134434340+Ven1c@users.noreply.github.com>
2023-09-18 22:34:39 +0300

    update 1.5

--- lab1.5.cpp
+++ lab1.5.cpp
@@ -1,49 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
-int main(){
-srand(time(NULL));
-int size=0;
-int w;
-scanf("%d",&size);
-scanf("%d",&w);
-int* mas;
-mas =(int*)malloc(sizeof(int*)*size);
-int size1=size;
-int i=0;
-int j=0;
-int s=0;
-int* mas1;
-int max=0;
-int min=1000;
-while(size1>i){
-	j=0;
-	mas1=mas;
-	mas1 = (int*)malloc(sizeof(int)*w);
-	while(w>j){
-		*mas1= rand() % 1000;
-		if (max<*mas1){
-			max=*mas1;
+int main() {
+	srand(time(NULL));
+	int size = 0;
+	int w;
+	int max = 0;
+	int min = 1000;
+	scanf("%d", &size);
+	scanf("%d", &w);
+	int** mas;
+	mas = (int**)malloc(sizeof(int*) * size);
+	int size1 = size;
+	int i = 0;
+	int j = 0;
+	int s = 0;
+	
+	
+	while (size1 > i) {
+		j = 0;
+		mas[i] = (int*)malloc(sizeof(int) * w);
+		while (w > j) {
+			mas[i][j] = rand() % 1000;
+			if (max < mas[i][j]) {
+				max = mas[i][j];
+			}
+			if (min > mas[i][j]) {
+				min = mas[i][j];
+			}
+			s += mas[i][j];
+			printf("%d\t", mas[i][j]);
+			j++;
 		}
-		if (min>*mas1){
-			min=*mas1;
-		}
-		s+=*mas1;
-		printf("%d\t",*mas1);
-		j++;
-		mas1++;
+		printf("| %d", s);
+		s = 0;
+		printf("\n");
+
+		i++;
 	}
-	printf("| %d",s);
-	s=0;
-	printf("\n");
-	mas1++;
-	i++;
-}
 
 
 
-printf("%d\t%d\n",max,min);
-max=max-min;
-printf("%d",max);
+	printf("%d\t%d\n", max, min);
+	max = max - min;
+	printf("%d", max);
 }
